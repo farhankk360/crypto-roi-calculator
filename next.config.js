c10c0bbc539b0532ab644354123311b4c1b750d0
farhan <farhan.kk360@hotmail.com>
2023-08-12 17:14:03 +0500

    fix: proxy headers test take 5

--- next.config.js
+++ next.config.js
@@ -7,7 +7,60 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
+const hubspotHeaders = [
+  { key: 'host', value: 'hubspot.cryptoroicalculator.com' },
+  {
+    key: 'X-HS-Public-Host',
+    value: 'hubspot.cryptoroicalculator.com',
+  },
+  {
+    key: 'X-HubSpot-Trust-Forwarded-For',
+    value: 'true',
+  },
+  {
+    key: 'X-Forwarded-Proto',
+    value: 'https',
+  },
+  {
+    key: 'x-hs-zone',
+    value: 'hscoscdn40.net',
+  },
+]
+
 module.exports = withPWA({
+  async rewrites() {
+    return [
+      {
+        source: '/funnels/sales/:path*',
+        destination: 'https://hubspot.cryptoroicalculator.com/sales/:path*',
+      },
+      {
+        source: '/funnels/application/:path*',
+        destination:
+          'https://hubspot.cryptoroicalculator.com/application/:path*',
+      },
+      {
+        source: '/(hs|_hcms|hubfs|hs-fs|cs/c|e3t)/:path*',
+        destination: 'https://hubspot.cryptoroicalculator.com/:path*',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(hs|_hcms|hubfs|hs-fs|cs/c|e3t)/:path*',
+        headers: hubspotHeaders,
+      },
+      {
+        source: '/funnels/sales/:path*',
+        headers: hubspotHeaders,
+      },
+      {
+        source: '/funnels/application/:path*',
+        headers: hubspotHeaders,
+      },
+    ]
+  },
   pwa: {
     dest: 'public',
     runtimeCaching,
