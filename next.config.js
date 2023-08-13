c8ed6fbfca016cebb1624b6b37d797e6e0fc4bb0
farhan <farhan.kk360@hotmail.com>
2023-08-13 13:22:46 +0500

    feat: hubspot poc removed

--- next.config.js
+++ next.config.js
@@ -8,18 +8,6 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
-  async rewrites() {
-    return [
-      {
-        source: '/funnels/sales/:path*',
-        destination: '/api/hubspot-proxy/sales/:path*',
-      },
-      {
-        source: '/(hs|_hcms|hubfs|hs-fs|cs/c|e3t)/:path*',
-        destination: '/api/hubspot-proxy/:path*',
-      },
-    ]
-  },
   pwa: {
     dest: 'public',
     runtimeCaching,
